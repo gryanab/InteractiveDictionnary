c4c6b4d1bcd0a5f21b352c1724d2a407b55fdb8e
Georges Ryan <gryan.ab@gmail.com>
2019-03-08 15:46:19 +0100

    Adds KeyError handling and case sensitivity
    - Accounting for non existing words 
    - Allowing inputs in all cases.

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ import json
 
 dictionary = json.load(open('data.json'))
 
+
 def definition(word):
-    return dictionary[word]
+    word_to_lower_case = word.lower()
+    try:
+        return dictionary[word_to_lower_case]
+    except KeyError:
+        return 'The definition you asked for seems to not exists in our database. Please double check the spelling.'
 
 
-word = input('Please enter the word you want to get the definition of: ')
+asked_word = input('Please enter the word you want to get the definition of: ')
 
-print(definition(word))
+print(definition(asked_word))
 
