b39493bc1e1b9264dee6ae925b65c7df592dc18c
Georges Ryan <gryan.ab@gmail.com>
2019-03-11 14:17:12 +0100

    app.py updates
    - adds doctype to function
    - updating confirmation from the user continuously
    - improving output.

--- app.py
+++ app.py
@@ -4,18 +4,48 @@ from difflib import get_close_matches
 dictionary = json.load(open('data.json'))
 
 
-def definition(word: str) -> str:
+def definition(word: str):
+    """
+    Returns definition of a single based on the data.json file.
+    Input: word as a string.
+    Output: Definition(s) as list.
+    """
+
     word_to_lower_case = word.lower()
     try:
         if word_to_lower_case in dictionary:
-            return str(dictionary[word_to_lower_case])
+            return dictionary[word_to_lower_case]
+        # returning several items in list
         elif len(get_close_matches(word_to_lower_case, dictionary.keys(), cutoff=0.8)) > 0:
-            return 'Did you mean {} instead?'.format(get_close_matches(word_to_lower_case, dictionary.keys())[0])
+            # proposing item with the greatest probability to match query. Hence, the first one on the list.
+            yn = str(input('Did you mean {} instead? yes/no : '.format(get_close_matches(word_to_lower_case, dictionary.keys())[0]))).lower()
+            while not(yn == 'y' or yn == 'yes' or yn == 'n' or yn == 'no'):
+                    # taking into account empty answer.
+                    if len(yn) == 0:
+                         print('Please enter yes/y or no/n')
+                    else:
+                        print('Please enter yes/y or no/n.')
+                    yn = str(input('Did you mean {} instead? yes/no : '.format(get_close_matches(word_to_lower_case, dictionary.keys())[0]))).lower().strip()
+            if yn == 'y' or yn == 'yes':
+                return dictionary[get_close_matches(word_to_lower_case, dictionary.keys())[0]]
+            elif yn == 'n' or yn == 'no':
+                return 'The definition you asked for seems to not exists in our database. Please double check the spelling.'
+        elif len(word) == 0:
+            return 'Please enter something.'
     except KeyError:
         return 'The definition you asked for seems to not exists in our database. Please double check the spelling.'
 
 
 asked_word = input('Please enter the word you want to get the definition of: ')
 
-print(definition(asked_word))
+output = definition(asked_word)
+
+if type(output) == list:
+    i = 1
+    for item in output:
+        print('Definition(s) of the word: ', asked_word)
+        print('Definition ', i, ' ', item)
+        i += 1
+else:
+    print(output)
 
